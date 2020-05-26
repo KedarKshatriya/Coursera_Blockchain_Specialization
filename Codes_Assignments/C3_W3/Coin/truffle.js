bd7f0486146b4e4d533eb850dbd5a8a963a5ca96
Kedar Kshatriya <kshatriyakedar@gmail.com>
2020-05-26 14:47:31 +0530

    Added all the assignment codes.

---
+++ Codes_Assignments/C3_W3/Coin/truffle.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+      solc: {
+         version: "0.4.17+commit.bdeb9e52.Emscripten.clang" 
+      }
+  }
+
+
+};
